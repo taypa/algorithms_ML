b6775964820fe81b2d6949e5510cfdbf2aa4f0e3
Ярослав Собещиков <sobeschikov10@gmail.com>
2024-07-15 15:33:29 +0800

    Change linear model

--- linear regression.py
+++ linear regression.py
@@ -124,17 +124,3 @@ class MyLineReg():
     @staticmethod
     def _mape(y: np.array, y_pred: np.array) -> float:
         return 100 * sum(abs((y-y_pred) / y)) / len(y)
-
-
-
-from sklearn.datasets import make_regression
-
-X, y = make_regression(n_samples=1000, n_features=14, n_informative=10, noise=15, random_state=42)
-X = pd.DataFrame(X)
-y = pd.Series(y)
-X.columns = [f'col_{col}' for col in X.columns]
-lin_reg = MyLineReg(n_iter = 50,
-        learning_rate = lambda iter: 0.5 * (0.85 ** iter),
-       )
-lin_reg.fit(X, y, 1)
-print(lin_reg.get_coef())
